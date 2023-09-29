c54d3ac885b97c6a4294ed61a0c6eab828b93966
Piotr <piotr.bania@europe.com>
2023-09-29 12:34:53 +0100

    init

---
+++ app/components/Page_Layout.js
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+const Page_Layout = ({ mainTitle, mainParagraph, imageSrc, imageAlt, secondaryTitle, secondaryParagraph, backgroundColor }) => {
+    return (
+        <section className='page' style={{ backgroundColor: backgroundColor }}>
+            <h1 className='heading_1'>{mainTitle}</h1>
+            <p className='paragraph_1'>{mainParagraph}</p>
+
+            <Image className='image' src={imageSrc} alt={imageAlt} width={1920} height={1200} />
+
+            <h1 className='text_shadow heading_2'>{secondaryTitle}</h1>
+            <p className='text_shadow paragraph_2'>{secondaryParagraph}</p>
+        </section>
+    )
+}
+
+export default Page_Layout
--- app/page.js
+++ app/page.js
@@ -1,16 +1,15 @@
-import './page.scss'
-import Image from 'next/image'
+import Page_Layout from './components/Page_Layout'
 
 export default function Home() {
     return (
-        <section className='page'>
-            <h1 className='heading_1'>Welcome to Elite Estates</h1>
-            <p className='paragraph_1'>Where Luxury Meets Elegance</p>
-
-            <Image className='image' src="/images/bedroom.jpg" alt='bedroom' width={1920} height={1200} />
-            
-            <h1 className='text_shadow heading_2'>Discover your dream home among our collection of extraordinary estates.</h1>
-            <p className='text_shadow paragraph_2'>With a focus on luxury and sophistication, Elite Estates brings you the finest real estate that exceeds every expectation.</p>
-        </section>
+        <Page_Layout 
+            mainTitle="Welcome to Elite Estates"
+            mainParagraph="Where Luxury Meets Elegance"
+            imageSrc="/images/bedroom.jpg"
+            imageAlt="bedroom"
+            secondaryTitle="Discover your dream home among our collection of extraordinary estates."
+            secondaryParagraph="With a focus on luxury and sophistication, Elite Estates brings you the finest real estate that exceeds every expectation."
+            backgroundColor="#F2F0E4"
+        />
     )
 }
